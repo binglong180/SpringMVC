18b72f953c85bbb7634a3e0374f41126d3faf0c4
binglong180 <907555993@qq.com>
2018-01-28 21:37:03 +0800

    使用RequestMapping传递参数

--- src/com/niu/springmvc/controller/IndexController.java
+++ src/com/niu/springmvc/controller/IndexController.java
@@ -6,27 +6,31 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
-@Controller
-public class IndexController extends AbstractController{
 
-	private Logger logger=Logger.getLogger(IndexController.class);
+@Controller
+public class IndexController extends AbstractController {
+
+	private Logger logger = Logger.getLogger(IndexController.class);
+
 	@Override
-	@RequestMapping("/index")
+	@RequestMapping(value = "/index")
 	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
 		// TODO Auto-generated method stub
-		//System.out.println("hello,SpringMvc");
+		// System.out.println("hello,SpringMvc");
 		logger.info("hello,SpringMvc");
 		return new ModelAndView("index");
 	}
-	@RequestMapping("/welcome")
-	public String welcome(@RequestParam String username){
-		logger.info("welcome,"+username);
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET, params = "username")
+	public String welcome(@RequestParam(value="username",required=false) String username) {
+		logger.info("welcome," + username);
 		return "index";
-		
+
 	}
 
 }
